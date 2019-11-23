e7ca1bde2b1d6e9c4e97fe348dd69a8b0a923954
Stefan <lazeastefan@gmail.com>
2019-11-24 00:22:50 +0200

    Progress 0%

--- DamApp/app/src/main/java/eu/ase/damapp/MainActivity.java
+++ DamApp/app/src/main/java/eu/ase/damapp/MainActivity.java
@@ -1,23 +1,45 @@
 package eu.ase.damapp;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.ArrayList;
+
+import eu.ase.damapp.fragment.HomeFragment;
+import eu.ase.damapp.util.Category;
 
 public class MainActivity extends AppCompatActivity {
-
+    NavigationView navigationView;
     DrawerLayout drawerLayout;
+    Fragment currentFragment;
+    ArrayList<Category> categories = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initCategories();
         initComponents();
     }
 
-    private void initComponents(){
+    private void initCategories(){
+        categories.add(new Category("Mecanica1", (float)5));
+        categories.add(new Category("Mecanica2", (float)3));
+        categories.add(new Category("Contraventii", (float)2));
+    }
+
+    private void initComponents() {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         drawerLayout = findViewById(R.id.drawer_layout);
@@ -30,5 +52,37 @@ public class MainActivity extends AppCompatActivity {
         );
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
+
+        navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(navigationItemSelectedListener());
+    }
+
+    private NavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener() {
+        return new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                if (menuItem.getItemId() == R.id.nav_home) {
+                    currentFragment = createHomeFragment();
+                }
+                openFragment();
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        };
+    }
+
+    private void openFragment() {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.main_frame_container, currentFragment)
+                .commit();
+    }
+
+    private Fragment createHomeFragment() {
+        Fragment fragment = new HomeFragment();
+        Bundle bundle = new Bundle();
+        bundle.putParcelableArrayList(HomeFragment.CATEGORY_KEY, categories);
+        fragment.setArguments(bundle);
+        return fragment;
     }
 }
--- DamApp/app/src/main/java/eu/ase/damapp/fragment/HomeFragment.java
+++ DamApp/app/src/main/java/eu/ase/damapp/fragment/HomeFragment.java
@@ -1,8 +1,57 @@
 package eu.ase.damapp.fragment;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.Toast;
+
 import androidx.fragment.app.Fragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import eu.ase.damapp.R;
+import eu.ase.damapp.util.Category;
+import eu.ase.damapp.util.CategoryAdapter;
 
 
 public class HomeFragment extends Fragment {
+    private ListView lvCategories;
+    private List<Category> categories = new ArrayList<>();
+    public static final String CATEGORY_KEY = "categoriesKey";
 
+
+    public HomeFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState
+    ) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        initComponents(view);
+        Toast.makeText(getContext(), "sunt in fragment", Toast.LENGTH_LONG).show();
+        return view;
+    }
+
+    private void initComponents(View view) {
+        lvCategories = view.findViewById(R.id.home_lv_categories);
+
+        if(getArguments()!=null){
+            categories = getArguments().getParcelableArrayList(CATEGORY_KEY);
+//            Toast.makeText(getContext(), categories.toString(), Toast.LENGTH_LONG).show();
+        }
+
+        if(getContext() != null){
+            CategoryAdapter adapter = new CategoryAdapter(getContext(),
+                    R.layout.lv_row_view,
+                    categories,
+                    getLayoutInflater());
+            lvCategories.setAdapter(adapter);
+            adapter.notifyDataSetChanged();
+        }
+    }
 }
